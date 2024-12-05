4b4f2ad94691187355e8509a6c77c16d45990591
shanthakumartk <shanthakumartk@gmail.com>
2024-12-05 17:00:33 +0530

    Create create-vm-content-library.ps1
    Updated with Cluster Name

---
+++ create-vm-content-library.ps1
@@ -0,0 +1,47 @@
+# PowerCLI script to Create Virtual Machine using Content Library Template in Scale
+# ==================================================================================
+#
+#  Mandatory Inputs needed for the script are vC Sever FDQN, vC Server Username, vC Password, VDS Portgroup Name, Datastore Name
+#  Resourcepool Name, Template, OS customization spec, and Number of Virtual Machines to be created
+#
+# Pre-req
+# VM Template & VM CustmomizatioSpec should be created upfront
+# Other Parameters have it handy
+#
+# Author: Shanthakumar K
+
+
+#Start of script.
+
+Param (
+    # Required parameters for the script.
+    [Parameter (Mandatory = $true, HelpMessage = 'Provide vCenter FQDN')] [ValidateNotNullOrEmpty()] [String]$vc_fqdn,
+    [Parameter (Mandatory = $true, HelpMessage = 'Provide vCenter SSO User')] [ValidateNotNullOrEmpty()] [String]$vc_username,
+    [Parameter (Mandatory = $true, HelpMessage = 'Provide vCenter SSO Password')] [ValidateNotNullOrEmpty()] [String]$vc_password,
+    [Parameter (Mandatory = $true, HelpMessage = 'Provide Virtual Machine Name')] [ValidateNotNullOrEmpty()] [String]$vmname,
+    [Parameter (Mandatory = $true, HelpMessage = 'Provide Portgroup Name')] [ValidateNotNullOrEmpty()] [String]$portgroupname,
+    [Parameter (Mandatory = $true, HelpMessage = 'Provide Datastore Name')] [ValidateNotNullOrEmpty()] [String]$datastorename,
+    [Parameter (Mandatory = $true, HelpMessage = 'Provide Cluster Name')] [ValidateNotNullOrEmpty()] [String]$clustername,
+    [Parameter (Mandatory = $true, HelpMessage = 'Provide Content Library Template Name')] [ValidateNotNullOrEmpty()] [String]$vmtemplate,
+    [Parameter (Mandatory = $true, HelpMessage = 'Provide Number of Virtual Machines to be created')] [ValidateNotNullOrEmpty()] [String]$numberofVMs
+)
+
+Write-Host "Connecting to vCenter Server"
+Write-Host "connect-viserver -Server $vc_fqdn -User $vc_username -Password $vc_password"
+connect-viserver -Server $vc_fqdn -User $vc_username -Password $vc_password
+
+Write-Host "Create Resource Pool under Cluster"
+$resourcepoolname = New-ResourcePool -Name Workload-VM -Location $clustername
+
+$numberofVM=1..$numberofVMs
+$numberofVM | foreach {
+
+Write-Host "New-VM -Name $vmname-$_ -Datastore $datastorename -ResourcePool $resourcepoolname -Template $vmtemplate -OSCustomizationspec $customizationspec"
+New-VM -Name $vmname-$_ -Datastore $datastorename -ResourcePool $resourcepoolname --ContentLibraryItem $vmtemplate -NetworkName $portgroupname
+
+Write-Host "Start-VM $vmname-$_"
+Start-VM $vmname-$_
+
+}
+
+#End of script.
